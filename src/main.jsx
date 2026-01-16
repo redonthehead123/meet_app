650e7157cd4fc2168e97558f2240ed4cc491b171
redonthehead123 <jordangreer1997@yahoo.com>
2026-01-16 03:22:16 -0600

    Remove Atatus Test

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import './index.css'
 
 import * as atatus from 'atatus-spa';
 atatus.config('fdf34e13bf7c4de4bca365b88b466f46').install();
-atatus.notify(new Error('Test Atatus Setup'));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
