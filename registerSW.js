e45d18254d9f4207d43f2e18c6a948b206cde24e
redonthehead123 <jordangreer1997@yahoo.com>
2026-01-19 10:07:41 -0600

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/meet_app/service-worker.js', { scope: '/meet_app/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/service-worker.js', { scope: '/' })})}
